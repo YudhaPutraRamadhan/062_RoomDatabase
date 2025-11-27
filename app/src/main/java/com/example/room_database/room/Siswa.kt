dd8100d1429d6bb94a36a467953dab83b8595704
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-27 14:49:41 +0900

    Mengubah file `Siswa.java` menjadi `Siswa.kt`.

--- app/src/main/java/com/example/room_database/room/Siswa.kt
+++ app/src/main/java/com/example/room_database/room/Siswa.kt
@@ -1,13 +1,13 @@
-package com.example.room_database.room;
+package com.example.room_database.room
 
-import androidx.room.Entity;
-import androidx.room.PrimaryKey;
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "tblSiswa")
 data class Siswa(
-        @PrimaryKey(autoGenerate = true)
-        val id : Int = 0,
-        val nama : String,
-        val alamat : String,
-        val telpon : String
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
 )
