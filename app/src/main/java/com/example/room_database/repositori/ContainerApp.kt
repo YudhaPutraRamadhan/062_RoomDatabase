8bdb360560f449ab06da9c927d04769a7935d18c
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-27 15:17:53 +0900

    Membuat `ContainerApp` untuk menyediakan RepositoriSiswa.

---
+++ app/src/main/java/com/example/room_database/repositori/ContainerApp.kt
@@ -0,0 +1,16 @@
+package com.example.room_database.repositori
+
+import android.content.Context
+import com.example.room_database.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+        override val repositoriSiswa: RepositoriSiswa by lazy {
+            OffLineRepositoriSiswa(
+                DatabaseSiswa.getDatabase(context).siswaDao())
+        }
+    }
