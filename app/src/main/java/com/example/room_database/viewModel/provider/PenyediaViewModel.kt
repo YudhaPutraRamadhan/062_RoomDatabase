8bfb30118a942f7b82879a4781a7b25496705b60
MzSuwawa <yudharamadhan838@gmail.com>
2025-12-04 14:01:34 +0900

    Menambahkan `DetailViewModel` ke `PenyediaViewModel`.

--- app/src/main/java/com/example/room_database/viewModel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/room_database/viewModel/provider/PenyediaViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import com.example.room_database.repositori.AplikasiSiswa
 import com.example.room_database.viewModel.EntryViewModel
 import com.example.room_database.viewModel.HomeViewModel
+import com.example.room_database.viewmodel.DetailViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -16,6 +18,9 @@ object PenyediaViewModel {
 
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }
