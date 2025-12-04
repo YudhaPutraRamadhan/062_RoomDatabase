7b8ebb5d72f6d5de2308381478cd4db56ad18dfc
MzSuwawa <yudharamadhan838@gmail.com>
2025-12-04 13:58:20 +0900

    Memperbaiki pemanggilan fungsi `delete` pada `SiswaDao`.

--- app/src/main/java/com/example/room_database/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/room_database/repositori/RepositoriSiswa.kt
@@ -19,6 +19,6 @@ class OffLineRepositoriSiswa(
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
-    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.getSiswa(siswa)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
 
